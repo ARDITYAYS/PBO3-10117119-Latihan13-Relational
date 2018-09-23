20e26c8311dd31a06d5593811823e863dacecddc
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:29:05 +0700

    first commit

---
+++ src/pbo310117119latihan13/PBO310117119Latihan13.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan13;
+/**
+ *
+ * @author 
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Fungsi dari Relasional
+ */
+public class PBO310117119Latihan13 {
+
+    public static void main(String[] args) {
+    int bil1 = 10;
+    int bil2 = 20;
+        System.out.println("a == b = " + (bil1==bil2));
+        System.out.println("a != b = " + (bil1!=bil2));
+        System.out.println("a > b = " + (bil1>bil2));
+        System.out.println("a < b = " + (bil1<bil2));
+        System.out.println("b >= a = "+(bil2>=bil1));
+        System.out.println("b <= a = " + (bil2<=bil1));
+    }
+    
+}
